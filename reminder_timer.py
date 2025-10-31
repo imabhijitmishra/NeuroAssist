1d849f0bcee74e5abe40d3e0c9a1a050ecd8913e
imabhijitmishra <abhijitmishra02@outlook.com>
2025-10-31 19:02:16 +0530

    Create reminder_timer.py

---
+++ reminder_timer.py
@@ -0,0 +1,19 @@
+import time
+from datetime import datetime
+
+medication_schedule = {
+    "08:00": "Donepezil",
+    "14:00": "Memantine",
+    "20:00": "Rivastigmine"
+}
+
+print("🧠 NeuroAssist Reminder Timer started...\n")
+
+while True:
+    now = datetime.now().strftime("%H:%M")
+    if now in medication_schedule:
+        medicine = medication_schedule[now]
+        print(f"💊 Reminder: It's {now}! Time to take your medicine: {medicine}")
+        time.sleep(60)
+    else:
+        time.sleep(30)
